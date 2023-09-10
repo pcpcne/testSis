18eb505e8114d9291f29c645f580de9db968430a
pcpcne <kandemirrmertt@gmail.com>
2023-09-10 16:29:16 +0300

    v5

--- src/main/java/org/testSis/Test001.java
+++ src/main/java/org/testSis/Test001.java
@@ -1,2 +1,8 @@
-package org.testSis;public class Test001 {
+package org.testSis;
+
+public class Test001 {
+    public static void main(String[] args) {
+            System.out.println("Hello amk");
+            System.out.println("Hello amk again");
+    }
 }
